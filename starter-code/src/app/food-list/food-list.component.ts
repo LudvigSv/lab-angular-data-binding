510c2bbbf53a0082343665249913e1510154a59d
Ludvig Svensson <ludvigsvensson@Ludvigs-MacBook-Air.local>
2018-03-06 18:57:34 -0500

    iteration 3 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,10 +8,23 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
   foodsArray: Object[];
+  newFood: Object = {};
+  todaysFood: Array<Object> = [];
 
   constructor() {}
 
   ngOnInit() {
     this.foodsArray = foods;
+  }
+  addFood() {
+    console.log('Add food has been called');
+    this.foodsArray.push(this.newFood);
+
+    // add contact to contacts list
+    // clear inputs
+  }
+  addToItemList(item) {
+    this.todaysFood.push(item);
+    console.log('my todays food list:', this.todaysFood);
   }
 }
